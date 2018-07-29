17de78301f3f2e1603a7d68682717739131aab1a
Wissam Nasser <wissam.nasser@student.ehb.be>
2018-07-29 18:55:43 +0200

    initial commit tabbed app
    /

---
+++ werkstuk1/werkstuk1/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  werkstuk1
+//
+//  Created by student on 29/07/2018.
+//  Copyright © 2018 student. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ werkstuk1/werkstuk1/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  werkstuk1
+//
+//  Created by student on 29/07/2018.
+//  Copyright © 2018 student. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
