fdf8bff86bfd8235f93b5fcc4f9cd5527c8135d3
Wissam Nasser <wissam.nasser@student.ehb.be>
2018-07-31 18:07:44 +0200

    folowing tutorial for table views
    https://www.youtube.com/watch?v=fFpMiSsynXM

--- Wissam_Nasser_digX_werkstuk1/Wissam_Nasser_digX_werkstuk1/FirstViewController.swift
+++ Wissam_Nasser_digX_werkstuk1/Wissam_Nasser_digX_werkstuk1/FirstViewController.swift
@@ -5,11 +5,28 @@
 //  Created by student on 31/07/2018.
 //  Copyright © 2018 student. All rights reserved.
 //
+//  Deze tutorial gevolgd voor de tableview controller : https://www.youtube.com/watch?v=fFpMiSsynXM
 
 import UIKit
 
-class FirstViewController: UIViewController {
+class FirstViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
+    var tablecells = PersonenData.data.getVips()
+    
+    public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int
+    {
+        return(tablecells.count)
+    }
+    
+    public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell
+    {
+        let cell = UITableViewCell(style: UITableViewCellStyle.default, reuseIdentifier: "vipCell")
+        cell.textLabel?.text = tablecells[indexPath.row].naam + " " + tablecells[indexPath.row].voornaam
+        cell.imageView?.image = tablecells[indexPath.row].foto
+        
+        return(cell)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
