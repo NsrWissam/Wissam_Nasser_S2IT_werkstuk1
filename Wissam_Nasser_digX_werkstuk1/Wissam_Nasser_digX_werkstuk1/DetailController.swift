63d3676d2acb7fc629ace05361d6e5bdd01e4027
Wissam Nasser <wissam.nasser@student.ehb.be>
2018-08-01 16:26:47 +0200

    Map + foto vergroot functionaliteit geimplementeerd

--- Wissam_Nasser_digX_werkstuk1/Wissam_Nasser_digX_werkstuk1/DetailController.swift
+++ Wissam_Nasser_digX_werkstuk1/Wissam_Nasser_digX_werkstuk1/DetailController.swift
@@ -4,30 +4,67 @@
 //
 //  Created by student on 31/07/2018.
 //  Copyright © 2018 student. All rights reserved.
-//
+//  Annotatie en implementatie kaart: https://www.youtube.com/watch?v=hRextIKJCnI
+//  Vergrote afbeelding weergave : https://stackoverflow.com/questions/34694377/swift-how-can-i-make-an-image-full-screen-when-clicked-and-then-original-size
 
 import UIKit
+import MapKit
 
 class DetailController: UIViewController {
 
-    
+    //view elements of detail view
     @IBOutlet weak var vipImage: UIImageView!
     @IBOutlet weak var vipVoornaam: UILabel!
     @IBOutlet weak var vipNaam: UILabel!
     @IBOutlet weak var vipTelefoonnummer: UILabel!
     @IBOutlet weak var vipAdres: UILabel!
     
+    @IBOutlet weak var vipMap: MKMapView!
+    
+    //object waar de segue variabelen worden in opgeslagen
     var vip = Persoon()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        //Map View configuratie
+        let span:MKCoordinateSpan = MKCoordinateSpanMake(0.1,0.1)
+        let location:CLLocationCoordinate2D = CLLocationCoordinate2DMake(vip.breedtegraad, vip.lengtegraad)
+        let region:MKCoordinateRegion = MKCoordinateRegionMake(location, span)
+        vipMap.setRegion(region, animated: true)
+        
+        let annotation = MKPointAnnotation()
+        annotation.coordinate = location
+        vipMap.addAnnotation(annotation)
+        
+        //doorgegeven VIP waardes worden gelinked aan view elements
         self.vipImage.image = vip.foto
         self.vipVoornaam.text = vip.voornaam
         self.vipNaam.text = vip.naam
         self.vipTelefoonnummer.text = vip.telefoonnummer
         self.vipAdres.text = vip.adres.straat + ", " + vip.adres.huisnummer + "\n" + vip.adres.postcode + " " + vip.adres.gemeente
     }
+    
+    @IBAction func imageTapped(_ sender: UITapGestureRecognizer) {
+        let imageView = sender.view as! UIImageView
+        let newImageView = UIImageView(image: imageView.image)
+        newImageView.frame = UIScreen.main.bounds
+        newImageView.backgroundColor = .black
+        newImageView.contentMode = .scaleAspectFit
+        newImageView.isUserInteractionEnabled = true
+        let tap = UITapGestureRecognizer(target: self, action: #selector(dismissFullscreenImage))
+        newImageView.addGestureRecognizer(tap)
+        self.view.addSubview(newImageView)
+        self.navigationController?.isNavigationBarHidden = true
+        self.tabBarController?.tabBar.isHidden = true
+    }
+    
+    @objc func dismissFullscreenImage(_ sender: UITapGestureRecognizer) {
+        self.navigationController?.isNavigationBarHidden = false
+        self.tabBarController?.tabBar.isHidden = false
+        sender.view?.removeFromSuperview()
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
     }
--- Wissam_Nasser_digX_werkstuk1/Wissam_Nasser_digX_werkstuk1/PersonenData.swift
+++ Wissam_Nasser_digX_werkstuk1/Wissam_Nasser_digX_werkstuk1/PersonenData.swift
@@ -16,11 +16,16 @@ class PersonenData {
     
     private init(){
         self.vips = [
-            Persoon(naam:"Obama",voornaam:"Barack",foto:#imageLiteral(resourceName: "Obama"),adres:Adres(straat:"Boekenberglei",huisnummer:"60",postcode:"2100",gemeente:"Deurne"),telefoonnummer:"+32475473815",lengtegraad:0.0, breedtegraad:0.0),
-            Persoon(naam:"Putin",voornaam:"Vladimir",foto:#imageLiteral(resourceName: "Putin"),adres:Adres(straat:"Zemlyanoy Val Street",huisnummer:"58",postcode:"109004",gemeente:"Moskava"),telefoonnummer:"+32457463815",lengtegraad:0.0, breedtegraad:0.0),
-            Persoon(naam:"Wissam",voornaam:"Nasser",foto:#imageLiteral(resourceName: "Meld"),adres:Adres(straat:"Quai de l'Industrie",huisnummer:"170",postcode:"1070",gemeente:"Anderlecht"),telefoonnummer:"+32447823815",lengtegraad:0.0, breedtegraad:0.0),
-            Persoon(naam:"Teresa",voornaam:"De Sock",foto:#imageLiteral(resourceName: "Julia"),adres:Adres(straat:"Mount St",huisnummer:"456",postcode:"LA 70806",gemeente:"Baton Rouge"),telefoonnummer:"+1 225-928-4655",lengtegraad:0.0, breedtegraad:0.0),
-            Persoon(naam:"Monica",voornaam:"Talous",foto:#imageLiteral(resourceName: "Hei"),adres:Adres(straat:"Hoogpoort",huisnummer:"1",postcode:"9000",gemeente:"Gent"),telefoonnummer:"+32477849864",lengtegraad:0.0, breedtegraad:0.0),
+            Persoon(naam:"Obama",voornaam:"Barack",foto:#imageLiteral(resourceName: "Obama"),adres:Adres(straat:"Boekenberglei",huisnummer:"60",postcode:"2100",gemeente:"Deurne")
+                ,telefoonnummer:"+32475473815",lengtegraad:3.766811299999972, breedtegraad:50.4084886),
+            Persoon(naam:"Putin",voornaam:"Vladimir",foto:#imageLiteral(resourceName: "Putin"),adres:Adres(straat:"Zemlyanoy Val Street",huisnummer:"58",postcode:"109004",gemeente:"Moskava")
+                ,telefoonnummer:"+32457463815",lengtegraad:3.7406806999999844, breedtegraad:51.0191803),
+            Persoon(naam:"Wissam",voornaam:"Nasser",foto:#imageLiteral(resourceName: "Meld"),adres:Adres(straat:"Quai de l'Industrie",huisnummer:"170",postcode:"1070",gemeente:"Anderlecht")
+                ,telefoonnummer:"+32447823815",lengtegraad:13.441443100000015, breedtegraad:52.5110751),
+            Persoon(naam:"Teresa",voornaam:"De Sock",foto:#imageLiteral(resourceName: "Julia"),adres:Adres(straat:"Mount St",huisnummer:"456",postcode:"LA 70806",gemeente:"Baton Rouge")
+                ,telefoonnummer:"+1 225-928-4655",lengtegraad:-119.20301770000003, breedtegraad:40.7886448),
+            Persoon(naam:"Monica",voornaam:"Talous",foto:#imageLiteral(resourceName: "Hei"),adres:Adres(straat:"Hoogpoort",huisnummer:"1",postcode:"9000",gemeente:"Gent")
+                ,telefoonnummer:"+32477849864",lengtegraad:18.281318499999998, breedtegraad:49.8204139),
         ]
     }
     
